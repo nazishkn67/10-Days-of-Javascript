54e62156dc6a7a4e31f1aa4a722622f2626bd838
nazishkn67 <40017618+nazishkn67@users.noreply.github.com>
2021-03-10 15:00:29 +0530

    file created

---
+++ Day 0/HelloWorld.js
@@ -0,0 +1,5 @@
+function greeting(parameterVariable) {
+   
+    console.log('Hello, World!');       //this line will print Hello, World! on the console
+    console.log(parameterVariable);     //this line will print value of parameterVariable on the console
+}
